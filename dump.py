bb6fcddd3907ad218fd941e7fa3b6895ce2a93a8
Alex Yursha <alexyursha@gmail.com>
2020-11-01 12:48:34 -1000

    Make denominator configurable

--- dump.py
+++ dump.py
@@ -12,11 +12,17 @@ c.execute("""
          n.id nutrient_id,
          n.name nutrient_name,
          n.unit_name nutrient_unit,
-         fn.amount nutrient_amount_per_100g
+         fn.amount nutrient_amount_per_100g,
+         (fn.amount * 100 / energy.amount) nutrient_amount_per_100kcal,
+         (fn.amount / fat.amount) nutrient_amount_per_1gfat
     FROM food_nutrient fn
       JOIN nutrient n ON fn.nutrient_id = n.id
       JOIN food f ON f.fdc_id = fn.fdc_id
       JOIN food_category fc ON f.food_category_id = fc.id
+      JOIN (SELECT fdc_id, amount FROM food_nutrient WHERE nutrient_id = 1008) energy
+           ON fn.fdc_id = energy.fdc_id
+      JOIN (SELECT fdc_id, amount FROM food_nutrient WHERE nutrient_id = 1004) fat
+           ON fn.fdc_id = fat.fdc_id
     WHERE f.food_category_id in (1, 2, 9, 11, 12, 15, 16, 20)
     ORDER BY (nutrient_amount_per_100g + 0) DESC
 """)
