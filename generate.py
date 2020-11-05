b5a199b941786f7bee50ae1ee56ea17e082c1b4b
Alex Yursha <alexyursha@gmail.com>
2020-11-04 17:09:25 -1000

    Separate deployment directory

--- generate.py
+++ generate.py
@@ -2,5 +2,5 @@ with open('data.json', 'r') as data_file:
   data = data_file.read()
   with open('index.html.tmpl', 'r') as f:
     html = f.read().replace('{{data}}', data)
-    with open('index.html', 'w') as output:
+    with open('public/index.html', 'w') as output:
       output.write(html)
